2e247316fce32036155b4f26725af2e4647c4621
Wing Yin Chan <66709835+wingchan852@users.noreply.github.com>
2023-05-06 15:19:28 -0700

    finished expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,39 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const dropdownMenu = document.getElementById('horn-select');
+  const hornAudio = document.querySelector('audio');
+  
+  dropdownMenu.addEventListener('change', (event) => {
+    const hornImage = document.querySelector('img');
+    hornImage.src = 'assets/images/' + event.target.value + '.svg';
+    hornAudio.src = 'assets/audio/' + event.target.value + '.mp3';
+  });
+
+  const volumeIcon = document.querySelector('div img');
+  const volumeLevel = document.getElementById('volume-controls');
+  // change volume icon & volume level based on the slider
+  volumeLevel.addEventListener('change', (event) => {
+    hornAudio.volume = event.target.value / 100;
+    if (event.target.value == 0) {
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+    } else if (event.target.value < 33) {
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+    } else if (event.target.value < 67) {
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+    } else {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+    }
+  });
+
+  // play audio when button is clicked
+  const jsConfetti = new JSConfetti();
+  const playButton = document.querySelector('button');
+  playButton.addEventListener('click', (event) => {
+    if (dropdownMenu.value == 'party-horn') {
+
+      jsConfetti.addConfetti();
+    }
+    hornAudio.play();
+  });
 }
